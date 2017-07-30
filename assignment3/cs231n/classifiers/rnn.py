a8850604b2e57abd82f6f5dbbec4cc76880176c2
CalciferZh <calciferzh@outlook.com>
2017-07-30 10:16:11 +0800

    finish RNN-Captioning

--- assignment3/cs231n/classifiers/rnn.py
+++ assignment3/cs231n/classifiers/rnn.py
@@ -220,7 +220,22 @@ class CaptioningRNN(object):
         # functions; you'll need to call rnn_step_forward or lstm_step_forward in #
         # a loop.                                                                 #
         ###########################################################################
-        pass
+        # def rnn_step_forward(x, prev_h, Wx, Wh, b):
+        captions = np.zeros((N, max_length), dtype='int32')
+        h, _ = affine_forward(features, W_proj, b_proj)
+        word_idx = np.array([self._start] * N)
+        for i in range(max_length):
+            # 1
+            word_v, _ = word_embedding_forward(word_idx.reshape(N, 1), W_embed)
+            # 2
+            h, _ = rnn_step_forward(word_v.reshape(N, -1), h, Wx, Wh, b)
+            # 3
+            scores = h.dot(W_vocab) + b_vocab
+            # 4
+            word_idx = np.argmax(scores, axis=1)
+            # save
+            captions[:,i] = word_idx
+        
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
